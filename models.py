4310e4504436426c971cd76339d416db3d3c5489
Kotaro Abe <abe@human.ait.kyushu-u.ac.jp>
2017-12-25 18:27:11 +0900

    [add] models.py, ops.py

---
+++ models.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+
+
+class Generator():
+
+    def __init__(self):
+        pass
+
+    def __call__(self):
+        pass
+
+
+class Discriminator():
+
+    def __init__(self):
+        pass
+
+    def __call__(self):
+        pass
